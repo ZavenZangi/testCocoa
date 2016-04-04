574cbe44270da4d4fe063ab4c75e06709b1bb95a
ZavenZangi <zaven.terteryan@zangi.com>
2016-04-04 15:58:49 +0400

    Initial Commit

---
+++ testCocoa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCocoa
+//
+//  Created by Zaven Terteryan on 4/4/16.
+//  Copyright © 2016 Grigori Jlavyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testCocoa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testCocoa
+//
+//  Created by Zaven Terteryan on 4/4/16.
+//  Copyright © 2016 Grigori Jlavyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
